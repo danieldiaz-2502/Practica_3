a4cffda440d1e036ea5f6a4ae0c7e48b42e655aa
danieldiaz-2502 <daniel_diazvalencia@outlook.com>
2020-09-03 23:21:35 -0500

    Todos los intents creo

--- app/src/main/java/com/example/practica_3/nota_activity.java
+++ app/src/main/java/com/example/practica_3/nota_activity.java
@@ -2,13 +2,44 @@ package com.example.practica_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class nota_activity extends AppCompatActivity {
+public class nota_activity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button calcularBoton;
+    private EditText proyecto1Edit;
+    private EditText proyecto2Edit;
+    private EditText quicesEdit;
+    private EditText parcial1Edit;
+    private EditText parcial2Edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nota_activity);
+
+        calcularBoton = findViewById(R.id.calcularBoton);
+        proyecto1Edit = findViewById(R.id.proyecto1Edit);
+        proyecto2Edit = findViewById(R.id.proyecto2Edit);
+        quicesEdit = findViewById(R.id.quicesEdit);
+        parcial1Edit = findViewById(R.id.parcial1Edit);
+        parcial2Edit = findViewById(R.id.parcial2Edit);
+
+        calcularBoton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.calcularBoton:
+                Intent i = new Intent(this, resultado_activity.class);
+                startActivity(i);
+                break;
+
+        }
     }
 }
--- app/src/main/java/com/example/practica_3/resultado_activity.java
+++ app/src/main/java/com/example/practica_3/resultado_activity.java
@@ -2,13 +2,38 @@ package com.example.practica_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class resultado_activity extends AppCompatActivity {
+public class resultado_activity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView notaFinal;
+    private Button otraVezBoton;
+    private TextView mensajeEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado_activity);
+
+        otraVezBoton = findViewById(R.id.otraVezBoton);
+        mensajeEdit = findViewById(R.id.mensajeEdit);
+        notaFinal = findViewById(R.id.notaFinal);
+
+        otraVezBoton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.otraVezBoton:
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+                break;
+        }
     }
 }
