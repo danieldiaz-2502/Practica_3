5761abd84f25fad6f0a1e7378d504f3499ee84dd
danieldiaz-2502 <daniel_diazvalencia@outlook.com>
2020-09-03 22:43:37 -0500

    algunos intents

--- app/src/main/java/com/example/practica_3/MainActivity.java
+++ app/src/main/java/com/example/practica_3/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.practica_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button azulBoton;
+    private Button blancoBoton;
+    private Button negroBoton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        azulBoton = findViewById(R.id.azulBoton);
+        negroBoton = findViewById(R.id.negroBoton);
+        blancoBoton = findViewById(R.id.blancoBoton);
+
+        azulBoton.setOnClickListener(this);
+        negroBoton.setOnClickListener(this);
+        blancoBoton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.azulBoton:
+                Intent i = new Intent(this, user_activity.class);
+                startActivity(i);
+                break;
+            case R.id.blancoBoton:
+                Intent e = new Intent(this, user_activity.class);
+                startActivity(e);
+                break;
+            case R.id.negroBoton:
+                Intent o = new Intent(this, user_activity.class);
+                startActivity(o);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/practica_3/user_activity.java
+++ app/src/main/java/com/example/practica_3/user_activity.java
@@ -2,13 +2,47 @@ package com.example.practica_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class user_activity extends AppCompatActivity {
+public class user_activity extends AppCompatActivity implements View.OnClickListener {
+
+    public Button configBoton;
+    public Button continuarBoton;
+    public EditText nombreEstudiante;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_activity);
+
+        configBoton = findViewById(R.id.configBoton);
+        continuarBoton = findViewById(R.id.continuarBoton);
+        nombreEstudiante = findViewById(R.id.nombreEstudiante);
+
+        configBoton.setOnClickListener(this);
+        continuarBoton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()){
+            case R.id.configBoton:
+                String nombre = nombreEstudiante.getText().toString();
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+                i.putExtra("nombre", nombre);
+                break;
+            case R.id.continuarBoton:
+                Intent e = new Intent(this, nota_activity.class);
+                startActivity(e);
+                break;
+        }
+
     }
 }
